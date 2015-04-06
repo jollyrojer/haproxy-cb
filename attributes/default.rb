a452ee350c929395c110b9983ec8f830dd5a43b2
Artem Ivantsov <aivantsov@qubell.com>
2015-04-06 12:51:07 +0300

    different backends for different path

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,4 @@ default['haproxy']['stats_user'] = "admin"
 default['haproxy']['stats_pass'] = "pa55w0rd"
 default['haproxy']['stats_uri'] = "/admin?stats"
 default['haproxy']['stats_port'] = "1926"
+default['haproxy']['entry_urls'] = [] 
