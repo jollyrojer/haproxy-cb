668dbe6e4f4898f1a5d3cc418915cebbb36e9dbe
Artem Ivantsov <aivantsov@qubell.com>
2015-04-06 22:12:04 +0300

    fix host in url

--- recipes/add_servers.rb
+++ recipes/add_servers.rb
@@ -14,9 +14,10 @@ end
 node.set['haproxy']['entry_urls'] = []
 node['haproxy']['server_uri'].map {|x| x=x+"/" if !x.include?("/"); x}.each do |server_uri|
   uri = server_uri.split("/", 2)[0]
+  host = node.cloud.public_hostname rescue node.ipaddress
   path = server_uri.split("/", 2)[1] 
   haproxy_bucket = "#{bucket_proto}#{node['haproxy']['bucket_type']}:#{node['haproxy']['bucket_port']}/#{path}"
-  node.set['haproxy']['entry_urls'] = node.haproxy.entry_urls | [ "#{bucket_proto}#{node['haproxy']['host']}:#{node['haproxy']['bucket_port']}/#{path}" ]
+  node.set['haproxy']['entry_urls'] = node.haproxy.entry_urls | [ "#{bucket_proto}#{host}:#{node['haproxy']['bucket_port']}/#{path}" ]
   bash "add server" do
     cwd "/usr/local/bin"
     code <<-EEND
